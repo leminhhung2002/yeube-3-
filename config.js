c150d97e40a9dfc87293a7a49af5240a974aeb44
leminhhung2002 <96687620+leminhhung2002@users.noreply.github.com>
2024-04-20 00:26:19 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = '';
+const giftUrl = 'Bé Yêu ';
+const eventName = 'Gửi tặng bé yêu ';
+const titleCard = 'Tặng Thế Giới Của No ❤️ ';
+const contentCard = 'Lúc đầu tớ nghĩ gặp cậu là một sự tình cờ ,nhưng cậu biết không có lẽ tớ đã giành hết may mắn vào sự tình cờ này .Bởi vì tớ đã từng một mình rất lâu trước đó,nên việc cậu xuất hiện như thế là một điều dịu dàng biết bao trong thế giới hàng tỷ người, giữa hàn vạn nỗi đau.Nếu có thể, tớ muốn hạnh phúc của tớ.Có Cậu ở trong đó. ❤️ ';
+
+const giftImage = 'beyeu.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
